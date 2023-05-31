ec0bc2cc900c2be29e47b060d4ce4cc1bf21d67d
Pearson <48200657+0xPearson@users.noreply.github.com>
2023-05-31 19:04:40 -0400

    upd

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -4,11 +4,6 @@ function App() {
   return (
     <>
       <div id="app">
-        <img
-          class="pearsonImage"
-          src="https://media.discordapp.net/attachments/922019115760230470/936884979022053376/Transparent.png?width=657&height=589"
-          alt=""
-        />
         <div class="welcome">
           DEVANTE.DEV REACT x VITE x ELECTRON BOILERPLATE
         </div>
